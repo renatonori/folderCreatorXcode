b2e710e32d112f6c2cd9c23b6bc0ebc363fa6ace
Renato Ioshida <renato.ioshida@mosyle.com>
2017-09-18 09:51:15 -0300

    Initial Commit

---
+++ FolderCreator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FolderCreator
+//
+//  Created by Mosyle01 on 18/09/17.
+//  Copyright © 2017 Mosyle. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
